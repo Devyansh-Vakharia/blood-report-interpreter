6efcac08cd17ae8118a67995f1340d973be3d110
Devyansh Vakharia <devyanshvakharia2510@gmail.com>
2025-01-24 22:15:23 +0530

    added expand functionality

--- app/static/script.js
+++ app/static/script.js
@@ -18,4 +18,26 @@ document.addEventListener("DOMContentLoaded", () => {
       selectedFileContainer.classList.add("hidden");
     });
   });
-  
+  const textarea = document.getElementById('messageInput');
+    const maxLines = 7;
+    const lineHeight = parseFloat(getComputedStyle(textarea).lineHeight);
+
+    textarea.addEventListener('input', function() {
+        if (textarea.value.trim() === '') {
+            textarea.style.height = '';  // Reset to default
+            textarea.style.overflowY = 'hidden';  // Hide scrollbar
+            textarea.rows = 1;
+            return;
+        }
+
+        const lines = Math.floor(textarea.scrollHeight / lineHeight);
+        
+        if (lines <= maxLines) {
+            textarea.style.height = 'auto'; 
+            textarea.style.height = lineHeight * lines + 'px';
+            textarea.style.overflowY = 'hidden'; 
+        } else {
+            textarea.style.height = lineHeight * maxLines + 'px';
+            textarea.style.overflowY = 'auto';  // Show scrollbar when max lines are reached
+        }
+    });
