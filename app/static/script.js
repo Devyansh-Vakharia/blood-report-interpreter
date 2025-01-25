35c1d1dff72cc84c5021437b34a29c5384957026
Abhishek Jaiswar <abhishekndxw@gmail.com>
2025-01-25 13:42:39 +0530

    refactor file upload and message input UI; enhance textarea auto-resizing

--- app/static/script.js
+++ app/static/script.js
@@ -1,43 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const fileInput = document.getElementById("file");
-    const selectedFileContainer = document.getElementById("selectedFileContainer");
-    const selectedFileName = document.getElementById("selectedFileName");
-    const removeFileButton = document.getElementById("removeFileButton");
-  
-    fileInput.addEventListener("change", (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        selectedFileName.textContent = file.name;
-        selectedFileContainer.classList.remove("hidden");
-      }
-    });
-  
-    removeFileButton.addEventListener("click", () => {
-      fileInput.value = ""; // Clear the file input
-      selectedFileName.textContent = "";
-      selectedFileContainer.classList.add("hidden");
-    });
-  });
+  const fileInput = document.getElementById("file");
+  const selectedFileContainer = document.getElementById("selectedFileContainer");
+  const selectedFileName = document.getElementById("selectedFileName");
+  const removeFileButton = document.getElementById("removeFileButton");
   const textarea = document.getElementById('messageInput');
-    const maxLines = 7;
-    const lineHeight = parseFloat(getComputedStyle(textarea).lineHeight);
 
-    textarea.addEventListener('input', function() {
-        if (textarea.value.trim() === '') {
-            textarea.style.height = '';  // Reset to default
-            textarea.style.overflowY = 'hidden';  // Hide scrollbar
-            textarea.rows = 1;
-            return;
-        }
+  fileInput.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      selectedFileName.textContent = file.name;
+      selectedFileContainer.classList.remove("hidden");
+    }
+  });
 
-        const lines = Math.floor(textarea.scrollHeight / lineHeight);
-        
-        if (lines <= maxLines) {
-            textarea.style.height = 'auto'; 
-            textarea.style.height = lineHeight * lines + 'px';
-            textarea.style.overflowY = 'hidden'; 
-        } else {
-            textarea.style.height = lineHeight * maxLines + 'px';
-            textarea.style.overflowY = 'auto';  // Show scrollbar when max lines are reached
-        }
-    });
+  removeFileButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    fileInput.value = "";
+    selectedFileName.textContent = "";
+    selectedFileContainer.classList.add("hidden");
+  });
+
+  textarea.addEventListener('input', function () {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+  });
+});
